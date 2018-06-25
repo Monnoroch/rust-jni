dd13e75a6c6df6c47e925b7845564e8427d3eddc
Monnoroch <monnoroch@gmail.com>
2018-06-25 21:54:53 +0100

    Implement to/from JNI conversions for primitive types.

--- src/jni.rs
+++ src/jni.rs
@@ -5,6 +5,8 @@ use java_string::*;
 use jni_sys;
 use raw::*;
 use std::cell::RefCell;
+use std::char;
+use std::iter;
 use std::marker::PhantomData;
 use std::os::raw::c_void;
 use std::ptr;
@@ -1507,7 +1509,8 @@ mod jni_env_tests {
 }
 
 /// A trait that represents a JNI type. It's implemented for all JNI primitive types
-/// and [`jni_sys::jobject`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jobject.html).
+/// and [`jobject`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jobject.html).
+/// Implements Java method calls and provides the default value for this JNI type.
 ///
 /// THIS TRAIT SHOULD NOT BE USED MANUALLY.
 ///
@@ -1522,7 +1525,16 @@ macro_rules! jni_type_trait {
     };
 }
 
+jni_type_trait!(jni_sys::jobject);
+jni_type_trait!(());
 jni_type_trait!(jni_sys::jboolean);
+jni_type_trait!(jni_sys::jchar);
+jni_type_trait!(jni_sys::jbyte);
+jni_type_trait!(jni_sys::jshort);
+jni_type_trait!(jni_sys::jint);
+jni_type_trait!(jni_sys::jlong);
+jni_type_trait!(jni_sys::jfloat);
+jni_type_trait!(jni_sys::jdouble);
 
 /// A trait that represents Rust types that are mappable to JNI types.
 /// This trait has to be implemented for all types that need to be passed as arguments
@@ -1573,15 +1585,34 @@ pub trait FromJni<'env>: JavaType {
     unsafe fn __from_jni(env: &'env JniEnv<'env>, value: Self::__JniType) -> Self;
 }
 
+/// Make references mappable to JNI types of their referenced types.
+impl<'a, T> JavaType for &'a T
+where
+    T: JavaType + ?Sized,
+{
+    #[doc(hidden)]
+    type __JniType = T::__JniType;
+}
+
+/// Make references mappable from JNI types of their referenced types.
+impl<'a, T> ToJni for &'a T
+where
+    T: ToJni,
+{
+    unsafe fn __to_jni(&self) -> Self::__JniType {
+        T::__to_jni(self)
+    }
+}
+
 /// Make [`bool`](https://doc.rust-lang.org/std/primitive.bool.html) mappable to
-/// [`jni_sys::jboolean`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jboolean.html).
+/// [`jboolean`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jboolean.html).
 impl JavaType for bool {
     #[doc(hidden)]
     type __JniType = jni_sys::jboolean;
 }
 
-/// Make [`bool`](https://doc.rust-lang.org/std/primitive.bool.html) convertable to
-/// [`jni_sys::jboolean`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jboolean.html).
+/// Make [`bool`](https://doc.rust-lang.org/std/primitive.bool.html) convertible to
+/// [`jboolean`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jboolean.html).
 impl ToJni for bool {
     unsafe fn __to_jni(&self) -> Self::__JniType {
         match self {
@@ -1591,8 +1622,8 @@ impl ToJni for bool {
     }
 }
 
-/// Make [`bool`](https://doc.rust-lang.org/std/primitive.bool.html) convertable from
-/// [`jni_sys::jboolean`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jboolean.html).
+/// Make [`bool`](https://doc.rust-lang.org/std/primitive.bool.html) convertible from
+/// [`jboolean`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jboolean.html).
 impl<'env> FromJni<'env> for bool {
     unsafe fn __from_jni(_: &'env JniEnv<'env>, value: Self::__JniType) -> Self {
         match value {
@@ -1622,6 +1653,436 @@ mod bool_tests {
         unsafe {
             assert_eq!(bool::__from_jni(&env, jni_sys::JNI_TRUE), true);
             assert_eq!(bool::__from_jni(&env, jni_sys::JNI_FALSE), false);
+        }
+    }
+
+    #[test]
+    fn to_and_from() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(bool::__from_jni(&env, true.__to_jni()), true);
+            assert_eq!(bool::__from_jni(&env, false.__to_jni()), false);
+        }
+    }
+
+    #[test]
+    fn from_and_to() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(
+                bool::__from_jni(&env, jni_sys::JNI_TRUE).__to_jni(),
+                jni_sys::JNI_TRUE
+            );
+            assert_eq!(
+                bool::__from_jni(&env, jni_sys::JNI_FALSE).__to_jni(),
+                jni_sys::JNI_FALSE
+            );
+        }
+    }
+}
+
+/// Make [`char`](https://doc.rust-lang.org/std/primitive.char.html) mappable to
+/// [`jchar`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jchar.html).
+impl JavaType for char {
+    #[doc(hidden)]
+    type __JniType = jni_sys::jchar;
+}
+
+/// Make [`char`](https://doc.rust-lang.org/std/primitive.char.html) convertible to
+/// [`jchar`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jchar.html).
+#[doc(hidden)]
+impl ToJni for char {
+    unsafe fn __to_jni(&self) -> Self::__JniType {
+        *self as Self::__JniType
+    }
+}
+
+/// Make [`char`](https://doc.rust-lang.org/std/primitive.char.html) convertible from
+/// [`jchar`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jchar.html).
+#[doc(hidden)]
+impl<'env> FromJni<'env> for char {
+    unsafe fn __from_jni(_: &'env JniEnv<'env>, value: Self::__JniType) -> Self {
+        let mut decoder = char::decode_utf16(iter::once(value));
+        // A character returned from Java is guaranteed to be a valid UTF-16 code point.
+        let character = decoder.next().unwrap().unwrap();
+        match decoder.next() {
+            None => {}
+            Some(second) => {
+                panic!(
+                    "Java character {:?} was mapped to more than one Rust characters: \
+                     [{:?}, {:?}, ...].",
+                    value, character, second,
+                );
+            }
+        }
+        character
+    }
+}
+
+#[cfg(test)]
+mod char_tests {
+    use super::*;
+
+    #[test]
+    fn to_jni() {
+        unsafe {
+            assert_eq!('h'.__to_jni(), 'h' as jni_sys::jchar);
+        }
+    }
+
+    #[test]
+    fn from_jni() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(char::__from_jni(&env, 'h' as jni_sys::jchar), 'h');
+        }
+    }
+
+    #[test]
+    fn to_and_from() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(char::__from_jni(&env, 'h'.__to_jni()), 'h');
+        }
+    }
+
+    #[test]
+    fn from_and_to() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(
+                char::__from_jni(&env, 'h' as jni_sys::jchar).__to_jni(),
+                'h' as jni_sys::jchar
+            );
+        }
+    }
+}
+
+/// A macro for generating [`JavaType`](trait.JavaType.html) implementations for most primitive
+/// Rust types.
+macro_rules! jni_io_traits {
+    ($type:ty, $jni_type:ty, $signature:expr, $doc:meta) => {
+        #[$doc]
+        impl JavaType for $type {
+            #[doc(hidden)]
+            type __JniType = $jni_type;
+        }
+
+        /// Make type convertible to the JNI type of the same size.
+        #[doc(hidden)]
+        impl ToJni for $type {
+            unsafe fn __to_jni(&self) -> Self::__JniType {
+                *self as Self::__JniType
+            }
+        }
+
+        /// Make type convertible from the JNI type of the same size.
+        #[doc(hidden)]
+        impl<'env> FromJni<'env> for $type {
+            unsafe fn __from_jni(_: &'env JniEnv<'env>, value: Self::__JniType) -> Self {
+                value as Self
+            }
+        }
+    };
+}
+
+jni_io_traits!(
+    (),
+    (),
+    "V",
+    doc = "Make [`()`](https://doc.rust-lang.org/std/primitive.unit.html) mappable to \
+           [`()`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jchar.html)."
+);
+jni_io_traits!(
+    u8,
+    jni_sys::jbyte,
+    "B",
+    doc = "Make [`u8`](https://doc.rust-lang.org/std/primitive.u8.html) mappable to \
+           [`jbyte`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jbyte.html)."
+);
+jni_io_traits!(
+    i16,
+    jni_sys::jshort,
+    "S",
+    doc = "Make [`i16`](https://doc.rust-lang.org/std/primitive.i16.html) mappable to \
+           [`jshort`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jshort.html)."
+);
+jni_io_traits!(
+    i32,
+    jni_sys::jint,
+    "I",
+    doc = "Make [`i32`](https://doc.rust-lang.org/std/primitive.i32.html) mappable to \
+           [`jint`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jint.html)."
+);
+jni_io_traits!(
+    i64,
+    jni_sys::jlong,
+    "J",
+    doc = "Make [`i64`](https://doc.rust-lang.org/std/primitive.i64.html) mappable to \
+           [`jlong`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jlong.html)."
+);
+// For some reason, floats need to be passed as 64-bit floats to JNI.
+// When passed as 32-bit numbers, Java recieves `0.0` instead of the passed number.
+// This also causes `__JniType` to not reside in `JavaType`, as this is the
+// only exceptional case.
+// TODO(#25): figure out the underlying cause of this.
+// native call -> java: f64
+// java -> native call: f32
+// java -> native method: f64
+// native method -> java: f64
+// jni_io_traits!(
+//     f32,
+//     jni_sys::jfloat,
+//     "F",
+//     doc = "Make [`f32`](https://doc.rust-lang.org/std/primitive.f32.html) mappable to \
+//            [`jfloat`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jfloat.html)."
+// );
+jni_io_traits!(
+    f64,
+    jni_sys::jdouble,
+    "D",
+    doc = "Make [`f64`](https://doc.rust-lang.org/std/primitive.f64.html) mappable to \
+           [`jdouble`](https://docs.rs/jni-sys/0.3.0/jni_sys/type.jdouble.html)."
+);
+
+#[cfg(test)]
+mod void_tests {
+    use super::*;
+
+    #[test]
+    fn to_jni() {
+        unsafe {
+            assert_eq!(().__to_jni(), ());
+        }
+    }
+
+    #[test]
+    fn from_jni() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(<()>::__from_jni(&env, ()), ());
+        }
+    }
+
+    #[test]
+    fn to_and_from() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(<()>::__from_jni(&env, ().__to_jni()), ());
+        }
+    }
+
+    #[test]
+    fn from_and_to() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(<()>::__from_jni(&env, ()).__to_jni(), ());
+        }
+    }
+}
+
+#[cfg(test)]
+mod byte_tests {
+    use super::*;
+
+    #[test]
+    fn to_jni() {
+        unsafe {
+            assert_eq!(217.__to_jni(), 217);
+        }
+    }
+
+    #[test]
+    fn from_jni() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(u8::__from_jni(&env, 217 as u8 as i8), 217);
+        }
+    }
+
+    #[test]
+    fn to_and_from() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(u8::__from_jni(&env, (217 as u8).__to_jni()), 217);
+        }
+    }
+
+    #[test]
+    fn from_and_to() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(
+                u8::__from_jni(&env, 217 as u8 as i8).__to_jni(),
+                217 as u8 as i8
+            );
+        }
+    }
+}
+
+#[cfg(test)]
+mod short_tests {
+    use super::*;
+
+    #[test]
+    fn to_jni() {
+        unsafe {
+            assert_eq!(217.__to_jni(), 217);
+        }
+    }
+
+    #[test]
+    fn from_jni() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(i16::__from_jni(&env, 217), 217);
+        }
+    }
+
+    #[test]
+    fn to_and_from() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(i16::__from_jni(&env, (217 as i16).__to_jni()), 217);
+        }
+    }
+
+    #[test]
+    fn from_and_to() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(i16::__from_jni(&env, 217).__to_jni(), 217);
+        }
+    }
+}
+
+#[cfg(test)]
+mod int_tests {
+    use super::*;
+
+    #[test]
+    fn to_jni() {
+        unsafe {
+            assert_eq!(217.__to_jni(), 217);
+        }
+    }
+
+    #[test]
+    fn from_jni() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(i32::__from_jni(&env, 217), 217);
+        }
+    }
+
+    #[test]
+    fn to_and_from() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(i32::__from_jni(&env, (217 as i32).__to_jni()), 217);
+        }
+    }
+
+    #[test]
+    fn from_and_to() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(i32::__from_jni(&env, 217).__to_jni(), 217);
+        }
+    }
+}
+
+#[cfg(test)]
+mod long_tests {
+    use super::*;
+
+    #[test]
+    fn to_jni() {
+        unsafe {
+            assert_eq!(217.__to_jni(), 217);
+        }
+    }
+
+    #[test]
+    fn from_jni() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(i64::__from_jni(&env, 217), 217);
+        }
+    }
+
+    #[test]
+    fn to_and_from() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(i64::__from_jni(&env, (217 as i64).__to_jni()), 217);
+        }
+    }
+
+    #[test]
+    fn from_and_to() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(i64::__from_jni(&env, 217).__to_jni(), 217);
+        }
+    }
+}
+
+#[cfg(test)]
+mod double_tests {
+    use super::*;
+
+    #[test]
+    fn to_jni() {
+        unsafe {
+            assert_eq!((217.).__to_jni(), 217.);
+        }
+    }
+
+    #[test]
+    fn from_jni() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(f64::__from_jni(&env, 217.), 217.);
+        }
+    }
+
+    #[test]
+    fn to_and_from() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(f64::__from_jni(&env, (217. as f64).__to_jni()), 217.);
+        }
+    }
+
+    #[test]
+    fn from_and_to() {
+        let vm = test_vm(ptr::null_mut());
+        let env = test_env(&vm, ptr::null_mut());
+        unsafe {
+            assert_eq!(f64::__from_jni(&env, 217.).__to_jni(), 217.);
         }
     }
 }
