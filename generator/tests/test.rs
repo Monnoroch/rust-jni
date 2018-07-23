fb30d5d87f77624a73ae21429b15e6615c787c6e
Monnoroch <monnoroch@gmail.com>
2018-07-23 12:32:27 +0100

    Support specifying a superclass in the generator.
    Related to #76.

--- generator/tests/test.rs
+++ generator/tests/test.rs
@@ -1,16 +1,24 @@
 #![allow(dead_code)]
+extern crate rust_jni;
 extern crate rust_jni_generator;
 
 #[cfg(test)]
-mod tests {
-    #[allow(unused_imports)]
-    use rust_jni_generator::*;
+mod a {
+    mod b {
+        use rust_jni::java;
 
-    java_generate!{
-        class a.b.TestClass1 {}
-        public class TestClass2 {}
+        #[allow(unused_imports)]
+        use rust_jni_generator::*;
+
+        java_generate! {
+            class a.b.TestClass1 extends java.lang.Object {}
+            public class TestClass2 extends TestClass1 {}
+        }
     }
+}
 
+#[cfg(test)]
+mod tests {
     #[test]
     fn test() {}
 }
