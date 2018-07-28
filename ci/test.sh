2bb7f899fece177fa9d2881d0345ad98aaf981ad
Monnoroch <monnoroch@gmail.com>
2018-07-28 12:40:30 +0100

    Reimplenment examples using the new generator.
    Related to #76.

--- ci/test.sh
+++ ci/test.sh
@@ -1,8 +1,8 @@
 set -e
 
 cargo test --verbose --features libjvm
-(cd examples/java-lib && ./test.sh)
 
 if [[ ${TRAVIS_RUST_VERSION} == "nightly" ]]; then
+	(cd examples/java-lib && ./test.sh)
 	(cd generator && cargo test --verbose)
 fi
