fd0d55a2195a8df07d10eb40cd93d0211e3f93a5
Monnoroch <monnoroch@gmail.com>
2019-11-24 18:50:21 +0000

    better CI

--- ci/install-java.sh
+++ ci/install-java.sh
@@ -12,3 +12,4 @@ sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
 
 # change JAVA_HOME to Java 8
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export LD_LIBRARY_PATH="$JAVA_HOME/jre/lib/amd64/server"
