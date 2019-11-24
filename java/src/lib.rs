d9ced2dcbddd4c2b99360bef4954b2056ce932d9
Max <monnoroch@gmail.com>
2019-11-24 21:36:38 +0000

    Upgrade the java package to use the cargo workspace. (#126)

--- java/src/lib.rs
+++ java/src/lib.rs
@@ -1,5 +1,8 @@
-extern crate rust_jni;
-
 pub mod lang {
+    //! Package java.lang.
+    //!
+    //! Provides classes that are fundamental to the design of the Java programming language.
+    //!
+    //! [`java.lang` javadoc](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/package-summary.html)
     pub use rust_jni::java::lang::*;
 }
