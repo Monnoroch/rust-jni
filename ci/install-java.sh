85361317cd3475003c131f2c58cdd5f72a651fbb
Monnoroch <monnoroch@gmail.com>
2019-11-24 19:26:11 +0000

    Find libjvm

--- ci/install-java.sh
+++ ci/install-java.sh
@@ -8,10 +8,3 @@ fi
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get -qq update
 sudo apt-get install -y openjdk-8-jdk --no-install-recommends
-
-# change JAVA_HOME to Java 8
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-find /usr/lib -name "libjvm.so"
-# TODO(https://github.com/rust-lang/cargo/issues/4895): remove this.
-export LD_LIBRARY_PATH="$JAVA_HOME/jre/lib/amd64/server"
