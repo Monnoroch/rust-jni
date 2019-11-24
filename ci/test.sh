26ef48a51af67ff18a5a5f7fd3f2e2afea06f136
Max <monnoroch@gmail.com>
2019-11-24 20:49:27 +0000

    Refactor the project into using a cargo workspace. (#125)

--- ci/test.sh
+++ ci/test.sh
@@ -6,4 +6,6 @@ cargo build --verbose
 cargo test --verbose --lib
 
 # All tests.
-cargo test --verbose --features libjvm
+# TOOD(https://github.com/rust-lang/cargo/issues/5015): stop cd-ing into individual
+# crates once the features bug is fixed.
+(cd rust-jni && cargo test --verbose --features libjvm)
--- test.sh
+++
@@ -1,7 +0,0 @@
-cargo build
-
-# No doc tests.
-cargo test --lib --tests
-
-# All tests.
-cargo test --features libjvm
